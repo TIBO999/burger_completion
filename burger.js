2c204a7f1dd0fd4deced0261aea84e742eb42473
Ibragim <81957008+TIBO999@users.noreply.github.com>
2022-06-17 12:19:14 +0300

    update

--- burger.js
+++ burger.js
@@ -1,186 +1,137 @@
-// создаем общий родительский класс
-class Food {
-    constructor(price, calories) {
-        this.price = price;
-        this.calories = calories;
+// создаем класс 'Hamburger'
+class Hamburger {
+    constructor () {
+        // ассоциативный массив данных связанный с размером
+        this.size = {
+            name: '',
+            price: 0,
+            calories: 0
+        };
+        // ассоциативный массив данных связанный с начинкой
+        this.stuffing = {
+            name: '',
+            price: 0,
+            calories: 0
+        };
+        // ассоциативный массив данных связанный с добавками
+        this.topping = {
+            name: [],
+            price: 0,
+            calories: 0
+        };
+        // цена бургера в сборе
+        this.price = 0;
+        // калорийность бургера в сборе
+        this.calories = 0;
+        // Узнать цену - запускаем расчет цены при создании объекта
+        this.calculateCalories();
+        // Узнать калорийность - запускаем расчет калорийности при создании объекта
+        this.calculatePrice();
     }
-    getPrice() {
-        return this.price;
-    }
-    getCalories() {
-        return this.calories;
-    }
-}
-// создаем дочерний класс - 'Burger' со свойством 'size'
-class Burger extends Food {
-    constructor(price, calories, size) {
-        super(price, calories);
-        this.size = size;
-        this.getSize();
-        this.getPrice();
-        this.getCalories();
-    }
+    // Узнать размер гамбургера
     getSize() {
+        this.size.name = [];
+        this.size.price = 0;
+        this.size.calories = 0;
         let arr = document.getElementsByName('size');
         for (let i = 0; i < arr.length; i++) {
             if (arr[i].checked) {
-                this.size = arr[i].value;
-                return this.size;
+                this.size.name = arr[i].value;
+                switch (this.size.name) {
+                    case 'big':
+                        this.size.price += 100;
+                        this.size.calories += 40;
+                        break;
+                    case 'small':
+                        this.size.price += 50;
+                        this.size.calories += 20;
+                        break;
+                    default:
+                        this.size.price += 0;
+                        this.size.calories += 0;
+                }
             }
         }
+        return this.size;
     }
-    getPrice() {
-        switch (this.size) {
-            case 'big':
-                this.price = 100;
-                break;
-            case 'small':
-                this.price = 50;
-                break;
-            default:
-                this.price = 0;
-        }
-        return this.price;
-    }
-    getCalories() {
-        switch (this.size) {
-            case 'big':
-                this.calories = 40;
-                break;
-            case 'small':
-                this.calories = 20;
-                break;
-            default:
-                this.calories = 0;
-        }
-        return this.calories;
-    }
-}
-// создаем дочерний класс - 'Stuffing' со свойством 'stuffing'
-class Stuffing extends Food {
-    constructor(price, calories, stuffing) {
-        super(price, calories);
-        this.stuffing = stuffing;
-        this.getStuffing();
-        this.getPrice();
-        this.getCalories();
-    }
+    // Узнать начинку гамбургера
     getStuffing() {
+        this.stuffing.name = [];
+        this.stuffing.price = 0;
+        this.stuffing.calories = 0;
         let arr = document.getElementsByName('stuffing');
         for (let i = 0; i < arr.length; i++) {
             if (arr[i].checked) {
-                this.stuffing = arr[i].value;
-                return this.stuffing;
+                this.stuffing.name = arr[i].value;
+                switch (this.stuffing.name) {
+                    case 'cheese':
+                        this.stuffing.price += 10;
+                        this.stuffing.calories += 20;
+                        break;
+                    case 'salad':
+                        this.stuffing.price += 20;
+                        this.stuffing.calories += 5;
+                        break;
+                    case 'potato':
+                        this.stuffing.price += 15;
+                        this.stuffing.calories += 10;
+                        break;
+                    default:
+                        this.stuffing.price += 0;
+                        this.stuffing.calories += 0;
+                }
             }
         }
+        return this.size;
     }
-    getPrice() {
-        switch (this.stuffing) {
-            case 'cheese':
-                this.price = 10;
-                break;
-            case 'salad':
-                this.price = 20;
-                break;
-            case 'potato':
-                this.price = 15;
-                break;
-            default:
-                this.price = 0;
-        }
-        return this.price;
-    }
-    getCalories() {
-        switch (this.stuffing) {
-            case 'cheese':
-                this.calories = 20;
-                break;
-            case 'salad':
-                this.calories = 5;
-                break;
-            case 'potato':
-                this.calories = 10;
-                break;
-            default:
-                this.calories = 0;
-        }
-        return this.calories;
-    }
-}
-// создаем дочерний класс - 'Topping' со свойством 'toppingType'
-class Topping extends Food {
-    constructor(price, calories, toppingType) {
-        super(price, calories);
-        this.toppingType = [];
-        this.getTopping();
-        this.getPrice();
-        this.getCalories();
-    }
-    getTopping() {
+    // Получить список добавок
+    getToppings() {
+        this.topping.name = [];
+        this.topping.price = 0;
+        this.topping.calories = 0;
         let arr = document.getElementsByName('topping');
         for (let i = 0; i < arr.length; i++) {
             if (arr[i].checked) {
-                this.toppingType.push(arr[i].value);
+                this.topping.name.push(arr[i].value);
+                this.topping.name.forEach((value, index) => {
+                    if (this.topping.name[index] == 'spices' && this.topping.price <= 0) {
+                        this.topping.price += 15;
+                        this.topping.calories += 0;
+                    }
+                    else
+                        if (this.topping.name[index] == 'mayonnaise') {
+                            this.topping.price += 20;
+                            this.topping.calories += 5;
+                        }
+                });
             }
         }
-        return this.toppingType;
+        return this.topping;
     }
-    getPrice() {
-        this.price = 0;
-        this.toppingType.forEach((value, index) => {
-            if (this.toppingType[index] == 'spices') {
-                this.price += 15;
-            }
-            else
-                if (this.toppingType[index] == 'mayonnaise') {
-                    this.price += 20;
-                }
-        });
+    // Узнать цену
+    calculatePrice() {
+        this.getSize()
+        this.getStuffing()
+        this.getToppings()
+        this.price = this.size.price + this.stuffing.price + this.topping.price;
+        document.querySelector('.price').innerHTML = `Your Burger Costs ${this.price} rubles`;
         return this.price;
     }
-    getCalories() {
-        this.calories = 0;
-        this.toppingType.forEach((value, index) => {
-            if (this.toppingType[index] == 'spices') {
-                this.calories += 0;
-            }
-            else
-                if (this.toppingType[index] == 'mayonnaise') {
-                    this.calories += 5;
-                }
-        });
+    // Узнать калорийность
+    calculateCalories() {
+        this.getSize()
+        this.getStuffing()
+        this.getToppings()
+        this.calories = this.size.calories + this.stuffing.calories + this.topping.calories;
+        document.querySelector('.calories').innerHTML = `Your Burger Has ${this.calories} calories`;
         return this.calories;
     }
 }
-// создаем класс - 'Hamburger' со свойствами 'sumPrice' и 'sumCalories'
-class Hamburger {
-    constructor() {
-        this.sumPrice = 0;
-        this.sumCalories = 0;
-        this.getSumPrice();
-        this.getSumCalories();
-    }
-    getSumPrice() {
-        let burg = new Burger();
-        let stuff = new Stuffing();
-        let topp = new Topping();
-        this.sumPrice = burg.price + stuff.price + topp.price;
-        document.querySelector('.bprice').innerHTML = `Your Burger Costs ${this.sumPrice} rubles`;
-        return this.sumPrice;
-    }
-    getSumCalories() {
-        let burg = new Burger();
-        let stuff = new Stuffing();
-        let topp = new Topping();
-        this.sumCalories = burg.calories + stuff.calories + topp.calories;
-        document.querySelector('.calories').innerHTML = `Your Burger Has ${this.sumCalories} calories`;
-        return this.sumCalories;
-    }
-}
-//создаем объект класса 'Humburger' и запускаем программу
+
+//создаем объект Humburger и запускаем программу
 let hum = new Hamburger();
 // пересчет Humburger по событию изменения радио и чекбоксов
 document.querySelectorAll('.ingredient').forEach(item => item.addEventListener('change', () => {
-    document.querySelector('.bprice').innerHTML = `Your Burger Costs ${hum.getSumPrice()} rubles`;
-    document.querySelector('.calories').innerHTML = `Your Burger Has ${hum.getSumCalories()} calories`;
+    document.querySelector('.price').innerHTML = `Your Burger Costs ${hum.calculatePrice()} rubles`;
+    document.querySelector('.calories').innerHTML = `Your Burger Has ${hum.calculateCalories()} calories`;
 }));
